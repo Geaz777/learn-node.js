82aa003aa1a0406f9ddc967b1a14670475bd1e15
Geaz777 <kimguhn777@gmail.com>
2022-05-25 14:50:09 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function workP(sec) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(new Data().toISOString());
+        }, sec * 1000);
+    
+    });
+}
+
+workP(1).then((result) => {
+    console.log('첫 번째 작업', result);
+    return workP(1);
+}).then((result) => {
+    console.log('두 번째 작업', result);
+});
